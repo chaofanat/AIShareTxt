697f42dc99a5a773e42090bcf5574b892f18c40b
chaofanat <chaofanat@gmail.com>
2025-11-06 16:26:35 +0800

    修复许可证分类器警告和版本号获取命令问题

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Financial and Insurance Industry",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
@@ -61,6 +60,7 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Topic :: Office/Business :: Financial :: Investment",
     ],
+    license="MIT",
     python_requires=">=3.10",
     install_requires=install_requires,
     extras_require={
